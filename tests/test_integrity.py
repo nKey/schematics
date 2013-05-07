3a9fdadcfa828e4df4e4c0c99a7cb1a3b66a3447
Paul Eipper <paul@nkey.com.br>
2013-05-07 15:25:08 -0300

    Validation integrity test

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -38,3 +38,49 @@ class TestDataIntegrity(unittest.TestCase):
         p1.id = 3
         self.assertRaises(ValidationError, p1.validate)
         self.assertEqual(p1.id, 4)
+
+    def test_dont_revalidate_valid_data(self):
+        """
+        A field should have a way to define data as already validated.
+
+        It would be difficult to deal with data that is not symmetrically
+        convertible otherwise.
+
+        In this example, data is validated and then mutated to an "encoded"
+        valid format for security purposes during conversion.
+
+        """
+        class PasswordType(StringType):
+            def __init__(self, *a, **kw):
+                self.sep = '|||'
+                self.alg = 'dummy'
+                super(PasswordType, self).__init__(*a, **kw)
+
+            def _dummy_hash(self, value):
+                # super secure way to encode a password
+                dummy_hash = str(abs(hash(value)))
+                return self.sep.join((self.alg, dummy_hash))
+
+            def _check_hash(self, value):
+                # validate the password is encoded securely
+                try:
+                    alg, digest = value.split(self.sep)
+                except ValueError:
+                    return False
+                return (alg == self.alg) and digest.isdigit()
+
+            def convert(self, value):
+                # password can come encoded or requiring encoding
+                value = super(PasswordType, self).convert(value)
+                if self.sep in value:
+                    return value  # already encoded, so it is valid
+                self.validate(value)
+                return self._dummy_hash(value)
+
+        class Player(Model):
+            password = PasswordType(min_length=6, max_length=6)
+
+        p1 = Player({'password': 'secret'})
+        self.assertNotEqual(p1.password, 'secret')
+        p1.validate()
+        self.assertNotIn('secret', p1.serialize()['password'])
